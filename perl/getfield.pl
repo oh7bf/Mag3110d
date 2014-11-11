a5e6a562c323cb1442ea83a622cbcb2ff252ca2a
Jaakko Koivuniemi <oh7bf@sral.fi>
2014-11-11 22:46:25 +0100

    Add Perl script to extract magnetometer data from log file.

---
+++ perl/getfield.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+#
+# Read mag3110d.log and extract magnetic field data from it.
+# Usage: getfield.pl < file
+#
+
+use strict;
+
+my $line="";
+
+print "time                 Bx     By     Bz\n";
+
+$line=<>;
+while($line) 
+{
+    printf "%4d-%02d-%02d %02d:%02d:%02d   %-+5d %-+5d %-+5d\n",$1,$2,$3,$4,$5,$6,$7,$8,$9 if($line =~ /(\d{4})-(\d{2})-(\d{2})\s(\d{2}):(\d{2}):(\d{2})\s+Bx=([+-]?\d{1,5})\s+By=([+-]?\d{1,5})\s+Bz=([+-]?\d{1,5}).*/);
+
+    $line=<>;
+}
+
+
